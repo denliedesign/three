7353fe37751632c1e59b544c241299391c93f256
denliedesign <customdenlie@gmail.com>
2025-03-22 09:50:37 -0500

    contact form security

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,23 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-
     public function sendEmail(Request $request)
     {
+        // Merge default values so honeypot fields are always present
+        $request->merge([
+            'my_name' => $request->input('my_name', ''),
+            'contact_me_by_fax_only' => $request->input('contact_me_by_fax_only', '')
+        ]);
+
+        // Validate the request data
+        $validated = $request->validate([
+            'name'                  => 'required|string|max:255',
+            'email'                 => 'required|email',
+            'message'               => 'required|string',
+            'submitted_at'          => 'required|numeric',
+            // Honeypot fields must be empty; max:0 ensures an empty string passes
+            'my_name'               => 'max:0',
+            'contact_me_by_fax_only'=> 'max:0'
+        ]);
+
+        // Honeypot timing check to deter bots
+        $submittedAt = $validated['submitted_at'];
+        if (Carbon::createFromTimestamp($submittedAt)->diffInSeconds(now()) < 5) {
+            return redirect('/')->with('message', 'Form submitted too quickly.');
+        }
+
+        // Prepare details for the email
         $details = [
-            'name' => $request->name,
-            'email' => $request->email,
-            'message' => $request->message
+            'name'                  => $validated['name'],
+            'email'                 => $validated['email'],
+            'message'               => $validated['message'],
+            'my_name'               => $validated['my_name'],
+            'contact_me_by_fax_only'=> $validated['contact_me_by_fax_only'],
+            'submitted_at'          => $validated['submitted_at']
         ];
 
-        Mail::to('studiothree.dance@gmail.com')->send(new ContactMail($details));
+        Mail::to('customdenlie@gmail.com')->send(new ContactMail($details));
 
         return redirect('/')->with('message', 'Thanks for your message. We\'ll be in touch.');
     }
-
 }
--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -1,6 +1,9 @@
 <form action="{{ route('contact.send') }}" method="POST" style="color: #747474;">
     @csrf
     <div class="mb-3">
+        <input type="hidden" name="my_name" id="my_name" value="">
+        <input type="checkbox" name="contact_me_by_fax_only" id="contact_me_by_fax_only" value="1" tabindex="-1" autocomplete="off" class="d-none">
+        <input type="hidden" name="submitted_at" value="{{ \Carbon\Carbon::now()->timestamp }}">
         <label for="name" class="col-form-label">Name</label>
         <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
         <div>{{ $errors->first('name') }}</div>
