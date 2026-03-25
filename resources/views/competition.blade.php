a6ae335c66d913f2ff084260e4e301cd448f035e
denliedesign <customdenlie@gmail.com>
2026-03-24 22:38:47 -0500

    pc updates

--- resources/views/competition.blade.php
+++ resources/views/competition.blade.php
@@ -11,9 +11,15 @@
                     <div class="col-sm-7 px-3 d-flex align-items-center">
                         <div>
                             <div style="font-size: 1.5em;" class="fw-bold pb-0 mb-0">S3D Performance Teams</div>
-                            <div class="text-muted mb-3" style="font-size: 0.9em;">$150 yearly team fee - ages 5 & up</div>
+                            <div class="text-muted mb-3" style="font-size: 0.9em;">$150 yearly team fee - Due August 1</div>
+                            <div class="badge bg-danger">Ages 5 & up</div>
+                            <div class="mb-3"><strong>JAZZ, TAP & HIP HOP TEAMS</strong></div>
                             <div>
-                                If you love to perform, consider joining one of our performance teams.  It’s a great fit for focused dancers looking for an additional challenge and performance opportunities. Dance Intensive Required for ALL company
+                                If you love to perform, consider joining one of our performance Jazz, Tap or Hip Hop teams. It’s a great fit for focused dancers looking for an additional challenge and performance opportunities.
+                                <br><br>
+                                Performance teams will perform at S3D’s Trunk or Treat, Dallas Mavericks pregame and attend 1 competition in April or May.
+                                <br><br>
+                                Performance team commitment is August 1, 2026 - May 31, 2027
                             </div>
                         </div>
                     </div>
@@ -25,12 +31,12 @@
         </section>
 
         <div class="my-5 py-5 text-center">
-                <a href="/images/company-team.pdf" target="_blank">
-                    <div class="btn btn-danger">Download the PDF</div>
-                </a>
-                    <img src="/images/company-team-1.jpg" alt="performance company team structure" class="img-fluid">
-                    <img src="/images/company-team-2.jpg" alt="performance company team structure" class="img-fluid">
-                    <img src="/images/company-team-3.jpg" alt="performance company team structure" class="img-fluid">
+{{--                <a href="/images/company-team.pdf" target="_blank">--}}
+{{--                    <div class="btn btn-danger">Download the PDF</div>--}}
+{{--                </a>--}}
+                    <img src="/images/pc-team-1.png" alt="performance company team structure" class="img-fluid">
+{{--                    <img src="/images/company-team-2.jpg" alt="performance company team structure" class="img-fluid">--}}
+                    <img src="/images/pc-team-3.jpg" alt="performance company team structure" class="img-fluid">
 
             </div>
         </div>
@@ -41,19 +47,19 @@
                     <div class="col-sm-7 px-3 d-flex align-items-center">
                         <div>
                             <div class="fw-bold" style="font-size: 1.5em;">S3D Elite Teams</div>
-                            <div class="text-muted mb-3" style="font-size: 0.9em;">$200 yearly team fee</div>
-                            <div class="badge bg-danger">Ages 5 & up</div>
-                            <div class="mb-3"><span class="fw-bold">BALLET/POINTE, TAP, JAZZ, LYRICAL/CONTEMPORARY, MUSICAL THEATER, ACRO TEAMS</span><br>REQUIRED: OKLA One Day Workshop and Company workshop & Auditions</div>
+                            <div class="text-muted mb-3" style="font-size: 0.9em;">$200 yearly team fee - Due Aug 1</div>
+                            <div class="badge bg-danger">Ages 6 & up</div>
+                            <div class="mb-3"><strong>MUSICAL THEATER, JAZZ, TAP, LYRICAL/CONTEMPORARY & HIP HOP TEAMS</strong></div>
                             <div>
-                                Competition teams will attend 3 Regional competitions February - May in the Dallas/Fort Worth area and 1 National Competition in Texas.  Elite team dancers are required to attend one dance convention each dance season.
+                                Elite competition dancers middle school ages and younger must first participate in our Musical Theatre large group/line or production before being placed on a small or large group competition team.
                                 <br><br>
-                                Elite competition dancers middle school-ages and younger must first participate in our Musical Theatre large group/line or production before being placed on a small or large group competition team.
+                                Elite competition dancers are required to take Ballet, Technique and Jazz in addition to Musical Theatre.  Other small and or large groups will be formed based on ability and interest in Musical Theatre, Jazz, Tap, Contemporary/Lyrical, and Hip Hop.
+                                Hip Hop Elite team dancers are required to take Move & Groove technique.
                                 <br><br>
-                                Elite competition dancers are required to take Ballet, Technique and Jazz in addition to the Musical Theatre group. Other small and or large groups will be formed based on ability and interest in Ballet/Pointe, tap, Contemporary, Musical Theatre, Acro and Hip Hop.  Hip Hop dancers are required to take Move & Groove technique for hip hop in addition to their team class.
+                                Competition teams will attend 3 Regional competitions February - May in the Dallas/Fort Worth area and 1 National Competition towards the end of June.  June classes are required before June Nationals.
                                 <br><br>
-                                Competitive team dancers are required to take June classes prior to StarQuest Nationals with classes on June 9, 11,16, 18. StarQuest will be held in Galveston June 21 - 26, 2026.
-                                <br><br>
-                                Performance Teams will attend 1 regional competition May 1-3 in Allen.
+                                Elite Team commitment is August 1, 2026 - June 30, 2027.
+
                             </div>
                         </div>
                     </div>
@@ -63,6 +69,12 @@
                 </div>
             </div>
         </div>
+    <div class="container mont mt-5">
+        <div class="text-center fw-bold" style="font-size: 1.5em;">
+            Auditions for the 2026-27 teams will be held at Studio 3 on
+            <br>Saturday, April 25th starting at 12:30.
+        </div>
+    </div>
 
     </div>
 
